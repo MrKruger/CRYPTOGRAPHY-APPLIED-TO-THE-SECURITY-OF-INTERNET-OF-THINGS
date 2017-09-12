7729560fbec39b1c7dd2308c065575c446f1118d
Gabriel <grk.82.gabriel@gmail.com>
2017-09-12 02:06:58 -0300

    Update CripIOT.py

--- CripIOT.py
+++ CripIOT.py
@@ -799,33 +799,33 @@ try:
 +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
 |                                                     HELP                                                     |
 +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
-
-|=> PARA ENCRIPTAR UM TEXTO, SELECIONE A OPÇÃO 1/TO ENCRYPT A TEXT, SELECT THE OPTION 1:
-    => EXEMPLO/EXAMPLE: >>> 1
-                [+] Digite um texto para ser Criptografado/Type a text to be Encrypted: xxxxxxxxxxx
-                [-] Encriptando/Encrypting...
-                [=] O resultado do texto Criptografado é/The result of the Encrypted text is: yyyyyyyyyyy
-
-    => OBSERVAÇÕES/REMARKS:
-        => O CÓDIGO ACEITA LETRAS SEM ACENTUAÇÃO E NÚMEROS/THE CODE ACCEPT UNLIMETED LETTERS AND NUMBERS.
-        => É POSSÍVEL ENCRIPTAR TEXTOS DE ATÉ 5625 CARACTERES POR VEZ/IT IS POSSIBLE TO ENCRYPT TEXTS OF UP
-           TO 5625 CHARACTERS ONCE.
-
-|=> PARA DECRIPTAR UM TEXTO, SELECIONE A OPÇÃO 2/TO DECRYPT A TEXT, SELECT THE OPTION 2:
-    => EXEMPLO/EXAMPLE: >>> 2
-                [+] Digite um texto para ser Decriptografado/Type a text to be Decrypted: yyyyyyyyyyy
-                [-] Decriptando/Decrypting...
-                [=] O resultado do texto Decriptografado é/The result of the Decrypted text is: xxxxxxxxxxx
-
-    => OBSERVAÇÕES/REMARKS:
-        => O TEXTO INSERIDO PARA DECRIPTAÇÃO DEVE OBRIGATÓRIAMENTE TER
-           SIDO ENCRIPTADO ANTES COM O MESMO ALGORÍTMO/THE TEXT INSERTED FOR DECRIPTION MUST BEEN ENCRYPTED
-           BEFORE THE SAME ALGORITHM.
-
-|=> PARA PEDIR AJUDA SELECIONE A OPÇÃO 3/TO ASK FOR HELP SELECT THE OPTION 3.
-
-|=> PARA VER OS CRÉDITOS SELECIONE A OPÇÃO 4/TO SEE THE CREDITS SELECT THE OPTION 4.
-
+|                                                                                                              |
+|=> PARA ENCRIPTAR UM TEXTO, SELECIONE A OPÇÃO 1/TO ENCRYPT A TEXT, SELECT THE OPTION 1:                       |
+|   => EXEMPLO/EXAMPLE: >>> 1                                                                                  |
+|               [+] Digite um texto para ser Criptografado/Type a text to be Encrypted: xxxxxxxxxxx            |
+|               [-] Encriptando/Encrypting...                                                                  |
+|               [=] O resultado do texto Criptografado é/The result of the Encrypted text is: yyyyyyyyyyy      |
+|                                                                                                              |    
+|   => OBSERVAÇÕES/REMARKS:                                                                                    |
+|       => O CÓDIGO ACEITA LETRAS SEM ACENTUAÇÃO E NÚMEROS/THE CODE ACCEPT UNLIMETED LETTERS AND NUMBERS.      |
+|       => É POSSÍVEL ENCRIPTAR TEXTOS DE ATÉ 5625 CARACTERES POR VEZ/IT IS POSSIBLE TO ENCRYPT TEXTS OF UP    |
+|          TO 5625 CHARACTERS ONCE.                                                                            |
+|                                                                                                              |
+|=> PARA DECRIPTAR UM TEXTO, SELECIONE A OPÇÃO 2/TO DECRYPT A TEXT, SELECT THE OPTION 2:                       |
+|   => EXEMPLO/EXAMPLE: >>> 2                                                                                  |
+|               [+] Digite um texto para ser Decriptografado/Type a text to be Decrypted: yyyyyyyyyyy          |
+|               [-] Decriptando/Decrypting...                                                                  |
+|               [=] O resultado do texto Decriptografado é/The result of the Decrypted text is: xxxxxxxxxxx    |
+|                                                                                                              |
+|   => OBSERVAÇÕES/REMARKS:                                                                                    |
+|       => O TEXTO INSERIDO PARA DECRIPTAÇÃO DEVE OBRIGATÓRIAMENTE TER                                         |
+|          SIDO ENCRIPTADO ANTES COM O MESMO ALGORÍTMO/THE TEXT INSERTED FOR DECRIPTION MUST BEEN ENCRYPTED    |
+|          BEFORE THE SAME ALGORITHM.                                                                          |
+|                                                                                                              |      
+|=> PARA PEDIR AJUDA SELECIONE A OPÇÃO 3/TO ASK FOR HELP SELECT THE OPTION 3.                                  |    
+|                                                                                                              |
+|=> PARA VER OS CRÉDITOS SELECIONE A OPÇÃO 4/TO SEE THE CREDITS SELECT THE OPTION 4.                           |
+|                                                                                                              |    
 +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
 |                                                     HELP                                                     |
 +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
